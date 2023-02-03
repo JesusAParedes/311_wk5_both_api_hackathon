44076da991698158fd969201e5a5f23133f141ba
JesusAParedes <paredesjesus93@gmail.com>
2023-02-02 20:31:58 -0600

    add function to get by id

--- controllers/employees.js
+++ controllers/employees.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const pool = require('../mysql/connection');
 const { errorOccurred } = require('../mysql/error');
 
@@ -14,7 +14,17 @@ const getEmployees = (req, res) => {
 };
 
 const getEmployeesById = (req, res) => {
-    res.send('getting employees...')
+    //sql statement
+    let sql = "select * from ?? where ?? = ?";
+    //variables that replace question marks in statement.
+    let rep = ['employees', 'emp_no', req.params.id];
+    //.format which places the variables in the sql statement
+    sql = mysql.format(sql,rep);
+    //query that runs to grab based off the req.params.id i.e. the emp_no the person looks for
+    pool.query(sql, (err, rows) => {
+        if (err) return errorOccurred(res, err)
+        return res.json(rows);
+    })
 };
 
 const getEmployeesByFirstName = (req,res) => {
