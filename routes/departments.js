0e18e27d81a8df75b7d43b86efd5728f4b2dd208
JesusAParedes <paredesjesus93@gmail.com>
2023-02-06 21:16:09 -0600

    add controller function, add department router and in index route. need to test

--- routes/departments.js
+++ routes/departments.js
@@ -1,11 +1,14 @@
 const express = require('express');
-const { departmentsController } = require('../controllers/departments');
+const departmentsController = require('../controllers/departments');
 const router = express.Router();
+
 //Default Route
 router.get('/', departmentsController.defaultRoute)
 //GET all departments
 router.get('/employees', departmentsController.getAllDepartments)
 
+
+router.put('/department/update', departmentsController.updateDepartment);
 
 // router.delete("/employees/:id", employeesController.deleteEmployeeById);
 // // delete Department by employee Id
