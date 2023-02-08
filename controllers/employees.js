1d64a03f09586c1d74dd45186f2541d8547c9396
Ayala1302 <marcoayala48@yahoo.com>
2023-02-07 20:39:56 -0600

    fixed updateEmployee reps

--- controllers/employees.js
+++ controllers/employees.js
@@ -54,7 +54,7 @@ const deleteDepartmentByEmployeeId = (req, res) => {
 // Add updateEmployee controller method
 const updateEmployee = (req, res) => {
   let sql = "UPDATE employees SET first_name = ?, last_name = ? WHERE ?? = ?";
-  let rep = [" ", " ", "emp_no", req.params.id];
+  let rep = [req.params.first_name, req.params.last_name, "emp_no", req.params.id];
   sql = mysql.format(sql, rep);
 
   pool.query(sql, (err, result) => {
