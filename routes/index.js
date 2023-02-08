580bfc463cba49f5992abdd27dac940d7273d0ef
NievesViri <105406390+NievesViri@users.noreply.github.com>
2023-02-07 19:51:02 -0600

    updated some code on employee.js

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,19 @@
-const express = require('express')
-const { appendFile } = require('fs')
-const router = express.Router()
-const employeesRoute = require('./employees.js')
+const express = require("express");
+const { appendFile } = require("fs");
+const router = express.Router();
+const employeesRoute = require("./employees.js");
 
-
-    //Default Route
-router.get('/', employeesRoute)
-    //GET all employees
-router.get('/employees', employeesRoute)
-    //GET employees by ID
-router.get('/employees/:id', employeesRoute)
-    //GET employees by First Name
-router.get('/employees/:fname', employeesRoute)
- //POST salary
- router.post('/salaries/add', salariesRoute)
+//Default Route
+router.get("/", employeesRoute);
+//GET all employees
+router.get("/employees", employeesRoute);
+//GET employees by ID
+router.get("/employees/:id", employeesRoute);
+//GET employees by First Name
+router.get("/employees/:fname", employeesRoute);
+//POST addEmployee
+router.post("/", employeesRoute);
+//POST salary
+router.post("/salaries/add", salariesRoute);
 
 module.exports = router;
